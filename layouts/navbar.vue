72907eb85a5749f62a99f2302dffdfd292e8ae94
nkc3g4 <microsoft5133@126.com>
2022-07-03 16:18:41 +0800

    chore

--- layouts/navbar.vue
+++ layouts/navbar.vue
@@ -1,7 +1,23 @@
 <template>
   <div>
-    Some shared layout content:
+    <a-input-search
+      v-model:value="value"
+      placeholder="input search text"
+      style="width: 200px"
+      @search="onSearch"
+    />
+
     <slot />
   </div>
 </template>
+<script lang="ts" setup>
+const value = ref<string>("");
 
+const onSearch = (searchValue: string) => {
+  // console.log("use value", searchValue);
+  // console.log("or use this.value", value.value);
+  navigateTo({
+    path: "/search/" + searchValue +"/1",
+  });
+};
+</script>
---
+++ pages/search/[query]/[page].vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <TheHeader />
+    <a-layout class="layout">
+      <a-layout-content style="padding: 0 50px; margin-top: 25px">
+        <div
+          :style="{ background: '#fff', padding: '24px', minHeight: '280px' }"
+        >
+          <TheFooter />
+        </div>
+      </a-layout-content>
+      <a-layout-footer style="text-align: center">
+        Ant Design ©2018 Created by Ant UED
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+<script lang="ts" setup>
+const route = useRoute();
+let query = route.params.query as string;
+let page = route.params.page;
+
+console.log(query, page);
+</script>
